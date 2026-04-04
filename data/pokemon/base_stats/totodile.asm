4ab184750c91768df00bbd68dc15c912a2f14ac6
dirkules41 <max.liebler.psn@gmail.com>
2026-04-04 18:16:01 +0200

    totodile.asm aktualisieren

--- data/pokemon/base_stats/totodile.asm
+++ data/pokemon/base_stats/totodile.asm
@@ -1,6 +1,6 @@
 db TOTODILE ; 158
 
-	db  50,  65,  64,  43,  44,  48
+	db  56,  62,  42,  60,  52,  42
 	;   hp  atk  def  spd  sat  sdf
 
 	db WATER, WATER ; type
@@ -13,7 +13,7 @@ db TOTODILE ; 158
 	db 5 ; unknown 2
 	INCBIN "gfx/pokemon/totodile/front.dimensions"
 	dw NULL, NULL ; unused (beta front/back pics)
-	db GROWTH_MEDIUM_SLOW ; growth rate
+	db GROWTH_SLOW ; growth rate
 	dn EGG_MONSTER, EGG_WATER_1 ; egg groups
 
 	; tm/hm learnset
